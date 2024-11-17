f71741ad2514f940b58dee317ad92c98eed4b391
Mohidworldwide <mohidssharif@gmail.com>
2024-11-17 20:51:18 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# # # file=open("xyz.txt","w")
+# # # file.write("i am creating a new file using python")
+
+
+# # # file=open("xyz.txt","r")
+# # # print(file.red())
+
+# # # file=open("xyz.txt","a")
+# # # file.write("appending")
+
+
+# # file=open("xyz.txt","r")
+
+# # print (file.read())
+# # print (file.readline())
+# # print (file.readline())
+# # print (file.readline())\
+
+# file=open("xyz.txt","r")
+# for line in file:
+#     print(line)
+
+
